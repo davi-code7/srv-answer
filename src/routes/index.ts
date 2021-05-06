9bc7bf483cc4572486a5e1a324a2a6ef4e7ceefb
Davi Silva <davi.silva@code7.com>
2021-05-06 14:31:52 -0300

    feat(status): adds status route
    adds status route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,9 @@
 import { Application } from 'express';
 
 import answer from './answer/answer';
+import status from './status/index';
 
 export default (app: Application): void => {
   app.use('/answer', answer);
+  app.use('/status', status);
 };
